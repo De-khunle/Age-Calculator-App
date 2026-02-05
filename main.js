b3bff652f377723b5f6d672883f74d9f3ada999a
De-khunle <popoolahabeeb9@gmail.com>
2026-02-05 01:27:23 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,116 +1,114 @@
-const dayInput = document.getElementById('input-1');
-const monthInput = document.querySelector('.input-2');
-const yearInput = document.getElementById('input-3');
-const iconBtn = document.querySelector('.icon');
-const spans = document.querySelectorAll('h1 span');
-const errorP = document.querySelectorAll('.error-p');
-const label = document.querySelectorAll('label');
+        const dayInput = document.getElementById('input-1');
+        const monthInput = document.querySelector('.input-2');
+        const yearInput = document.getElementById('input-3');
+        const iconBtn = document.querySelector('.icon');
+        const span = document.querySelectorAll('h1 span');
+        const errorP = document.querySelectorAll('.error-p');
+        const label = document.querySelectorAll('label');
 
-//for loop for individual input border-color-red
-[dayInput, monthInput, yearInput].forEach(input => {
-    input.addEventListener('input', () => {
-        input.classList.remove('input-error');
-    })
-});
+        //for-loop for individual input style
+        [dayInput, monthInput, yearInput].forEach((input, index) => {
+            input.addEventListener('click', () => {
+                input.classList.remove('input-error');
+                errorP[index].classList.remove('t-display');
+                label[index].classList.remove('label-error');
+            })
+        });
+        
+        iconBtn.addEventListener('click', () => {
+            const birthDay = Number(dayInput.value);
+            const birthMonth = Number(monthInput.value) -1;
+            const birthYear = Number(yearInput.value);
 
-iconBtn.addEventListener('click', function () {
-    const birthDay = Number(dayInput.value);
-    const birthMonth = Number(monthInput.value) - 1;
-    const birthYear = Number(yearInput.value);
-    const today = new Date();
+            const today = new Date();
 
-    let hasError = false;
-    function getDaysInMonth(year, month) {
-        return new Date(year, month, 0).getDate();
-    }
+            const currentDay = today.getDate(); 
+            const currentMonth = today.getMonth(); 
+            const currentYear = today.getFullYear(); 
+
+            function getDaysInMonth(year, month) {
+                return new Date(year, month, 0).getDate();
+            }
+
+            let hasError = false;
+            // empty Input value
+            if (dayInput.value === '') {
+                dayInput.classList.add('input-error');
+                errorP[0].classList.add('t-display');
+                label[0].classList.add('label-error');
+                hasError = true;
+            }
+            if (monthInput.value === '') {
+                monthInput.classList.add('input-error');
+                errorP[1].classList.add('t-display');
+                label[1].classList.add('label-error');
+                hasError = true;
+            }
+            if (yearInput.value === '') {
+                yearInput.classList.add('input-error');
+                errorP[2].classList.add('t-display');
+                label[2].classList.add('label-error');
+                hasError = true;
+            }
+            if(hasError){return;};
+
+            //error for invalid day
+            if (birthDay < 1 || birthDay > 31) {
+                dayInput.classList.add('input-error');
+                errorP[0].textContent = 'must be a valid day'
+                errorP[0].classList.add('t-display');
+                label[0].classList.add('label-error');
+                return;
+            }
+            //error for invalid month
+            const rawwMonth = Number(monthInput.value);
+            if (rawwMonth < 1 || rawwMonth > 12) {
+                monthInput.classList.add('input-error');
+                errorP[1].textContent = 'must be a valid month';
+                errorP[1].classList.add('t-display');
+                label[1].classList.add('label-error');
+                return;
+            }
+
+            //error for days that don't exist i.e feb 30
+            const daysInMonth = getDaysInMonth(birthYear, birthMonth + 1);
+            if (birthDay > daysInMonth) {
+                dayInput.classList.add('input-error');
+                errorP[0].textContent = 'must be a valid date';
+                errorP[0].classList.add('t-display');
+                label[0].classList.add('label-error');
+                return;
+            }
+            //error for future date check
+            const birthDate = new Date(birthYear, birthMonth, birthDay);
+            if (birthDate > today) {
+                yearInput.classList.add('input-error');
+                errorP[2].textContent = 'must be in the past';
+                errorP[2].classList.add('t-display');
+                label[2].classList.add('label-error');
+                return;
+            }
 
 
-    //remove error as user type
-    if (dayInput.value === '') {
-        dayInput.classList.add('input-error');
-        errorP[0].classList.add('t-display');
-        label[0].style.color = '#ff5757';
-        hasError = true;
-    } 
-    if (monthInput.value=== '') {
-        monthInput.classList.add('input-error');
-        errorP[1].classList.add('t-display');
-        label[1].style.color = '#ff5757';
-        hasError = true;
-    } 
-    if (yearInput.value === '') {
-        yearInput.classList.add('input-error');
-        errorP[2].classList.add('t-display');
-        label[2].style.color = '#ff5757';
-        hasError = true;
-    } 
-    if(hasError) return;
+            let ageYears = currentYear - birthYear;
+            let ageMonths = currentMonth - birthMonth;
+            let ageDays = currentDay - birthDay;
 
-//invalid day input error
-if (birthDay < 1 || birthDay > 31) {
-        dayInput.classList.add('input-error');
-        errorP[0].textContent = 'Must be a valid day';
-        errorP[0].classList.add('t-display');
-        label[0].style.color = '#ff5757';
-        // alert('Day must be between 1 and 31');
-        return;
-    }
-    //invalid month input error
-    const rawMonth = Number(monthInput.value)
-    if (rawMonth < 1 || rawMonth > 12) {
-    monthInput.classList.add('input-error');
-    errorP[1].textContent = 'Must be a valid month';
-    errorP[1].classList.add('t-display');
-    label[1].style.color = '#ff5757';
-    // alert('Month must be between 1 and 12');
-    return;
-}
-//for dates that do not exist i.e feb 30
-const daysInMonth = getDaysInMonth(birthYear, birthMonth + 1);
-if (birthDay > daysInMonth) {
-        dayInput.classList.add('input-error');
-        monthInput.classList.add('input-error');
-        yearInput.classList.add('input-error');
-        errorP[0].textContent = 'Must be a valid day';
-        errorP[0].classList.add('t-display');
-        label[0].style.color = '#ff5757';
-        label[1].style.color = '#ff5757';
-        label[2].style.color = '#ff5757';
-    // alert('Invalid date for the selected month');
-    return;
-}
-//error for future date check
-const birthDate = new Date(birthYear, birthMonth, birthDay);
-if(birthDate > today) {
-    yearInput.classList.add('input-error');
-    errorP[2].textContent = 'Must be in the past';
-    errorP[2].classList.add('t-display');
-    label[2].style.color = '#ff5757';
-    // alert('Date cannot be in the future');
-    return;
-}
-    
-    const currentDay = today.getDate();
-    const currentMonth = today.getMonth();
-    const currentYear = today.getFullYear();
-    
-    let ageYears = currentYear - birthYear;
-    let ageMonths = currentMonth - birthMonth;
-    let ageDays = currentDay - birthDay;
-      
-    if (ageDays < 0) {
-        ageMonths--;
-        ageDays += getDaysInMonth(currentYear, currentMonth -1);
-    }
-    if (ageMonths < 0) {
-        ageYears--;
-        ageMonths += 12;
-    }
-    displayAge(ageYears, ageMonths, ageDays);
-});
+            if (ageDays < 0) {
+                ageMonths --;
+                ageDays += getDaysInMonth(currentYear, currentMonth)
+            }
 
-function displayAge(years, months, days) {
-    spans[0].textContent = years;
-    spans[1].textContent = months;
-    spans[2].textContent = days;
-}
+            if (ageMonths < 0) {
+                ageYears--;
+                ageMonths += 12;
+            }
+
+            displayAge(ageYears, ageMonths, ageDays);
+        });
+
+        function displayAge(years, months, days) {
+            span[0].textContent = years;
+            span[1].textContent = months;
+            span[2].textContent = days;
+        }
